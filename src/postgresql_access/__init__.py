095d80a6e1fa99bbd15a788ba8dd79fab8eb8b0b
Gerard Weatherby <gweatherby@uchc.edu>
2025-05-02 10:53:00 -0400

    AbstractDatabase to module level

--- src/postgresql_access/__init__.py
+++ src/postgresql_access/__init__.py
@@ -5,5 +5,5 @@ postgresql_access_logger = logging.getLogger(__name__)
 
 __version__ = importlib.metadata.version('postgresql_access')
 
-from postgresql_access.database import DatabaseConfig, DatabaseDict, ReadOnlyCursor, NewTransactionCursor
+from postgresql_access.database import AbstractDatabase, DatabaseConfig, DatabaseDict, ReadOnlyCursor, NewTransactionCursor
 from postgresql_access.database import SelfCloseCursor, SelfCloseConnection
